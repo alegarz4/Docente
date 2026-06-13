8df67b071d38ec99e6a0fcd18657dd97454d33ff
alegarz4 <mtra.claudiaalejandragarza@gmail.com>
2026-06-13 08:58:20 -0600

    Publica actualización de reportes grupales

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "docente-pwa-v9";
+const CACHE_NAME = "docente-pwa-v10";
 const APP_FILES = ["./","./index.html","./manifest.webmanifest","./icon.svg","./report-search.js","./period-averages.js","./school-improvements.js"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_FILES)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
